8206c60e6631a34bdd652b3e740abf8c1caca9d6
Zebin Zhou <zb.zhou0922@gmail.com>
2022-01-17 20:44:23 -0800

    C_to_F

---
+++ CtoF.py
@@ -0,0 +1,7 @@
+# Celsius to Fahrenheit convertor.
+c_degree = input('Please enter the Celsius degree: ')
+c_degree = float(c_degree)
+
+f_degree = c_degree * 9 / 5 + 32
+
+print('The result in Fahrenheit is ', f_degree)
